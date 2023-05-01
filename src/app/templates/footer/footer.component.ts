ebb98608845971aa2f5728248fa5f50609a7b6de
memo124 <48780097+memo124@users.noreply.github.com>
2023-05-01 08:54:59 -0600

    added new design footer and header

--- src/app/templates/footer/footer.component.ts
+++ src/app/templates/footer/footer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  public anio:number = 0;
+
+  ngOnInit(){
+    var today = new Date();
+    this.anio = today.getFullYear()
+  }
 
 }
