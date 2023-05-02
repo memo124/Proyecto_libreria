b496c43ff0d7f8a2ed6d1e98ef38d1a2dd980e5e
memo124 <48780097+memo124@users.noreply.github.com>
2023-05-01 22:14:14 -0600

    Added modal

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-books',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
+
+  constructor(private sanitizer: DomSanitizer) {
+
+  }
 
   public menus: object = [
     {option:"book"},
@@ -17,5 +22,43 @@ export class BooksComponent {
     {option:"reserve"},
     {option:"user"}
   ];
+
+  public previsualizacion?: string;
+  public archivos: any = [];
+  public img: string = "";
+  public base64:any;
+  capturarFile(event:any):any{
+    this.img = event.target.files[0].name;
+    const archivoCapturado = event.target.files[0]
+    this.extraerBase64(archivoCapturado).then((imagen: any) => {
+      this.previsualizacion = imagen.base;
+    })
+    this.archivos.push(archivoCapturado)
+  }
+
+  extraerBase64 = async ($event: any) => new Promise((resolve) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        this.base64 = reader.result;
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          base: null
+        });
+      };
+
+      return ;
+
+    } catch (e) {
+      return null;
+    }
+  })
 
 }
