b7745a78206e06add82fad116b895c309f6f6267
memo124 <48780097+memo124@users.noreply.github.com>
2023-05-23 19:28:54 -0600

    Add new funcionality of Genesis

--- src/app/pages/editorial/editorial.component.ts
+++ src/app/pages/editorial/editorial.component.ts
@@ -1,22 +1,97 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { classHelper } from 'src/app/helpers/helper';
 import { editorialI } from 'src/app/interfaces/editorial.interface';
 import { EditorialService } from 'src/app/services/editorial/editorial.service';
 
 @Component({
   selector: 'app-editorial',
   templateUrl: './editorial.component.html',
-  styleUrls: ['./editorial.component.css']
+  styleUrls: ['./editorial.component.css'],
 })
 export class EditorialComponent implements OnInit {
-
   public editorials: editorialI[] = [];
-  public filterName:string = "";
-  constructor(public editorialS:EditorialService){}
+  public filterName: string = '';
+  public dataEditorial: editorialI;
+  public formEditorial: FormGroup;
+  public idEditorial: number = 0;
 
-  ngOnInit(): void {
-    this.editorialS.getEditorial().subscribe(data=>{
-      this.editorials = data
+  constructor(
+    private editorialS: EditorialService,
+    private helper: classHelper
+  ) {
+    this.formEditorial = new FormGroup({
+      idEditorial: new FormControl('', Validators.required),
+      nameEditorial: new FormControl('', [
+        Validators.pattern(this.helper.validateSting()),
+      ]),
+      dateAdd: new FormControl(),
+      statusEditorial: new FormControl(),
     });
   }
 
+  ngOnInit(): void {
+    this.getAlls();
+  }
+
+  getAlls() {
+    this.editorialS.getEditorial().subscribe((editorial) => {
+      this.editorials = editorial;
+    });
+  }
+
+  sendForm(form: object) {
+    if (this.idEditorial != 0) {
+      this.editorialS
+        .putEditorial(form, this.idEditorial)
+        .subscribe((editorial) => {
+          this.helper.messageAlert(
+            'Successfully',
+            editorial.response,
+            'success',
+            'Accepted'
+          );
+          this.getAlls();
+        });
+    } else {
+      this.editorialS.postEditorial(form).subscribe((editorial) => {
+        this.helper.messageAlert(
+          'Successfully',
+          editorial.response,
+          'success',
+          'Accepted'
+        );
+        this.getAlls();
+      });
+    }
+  }
+
+  viewStatus(status: boolean) {
+    return this.helper.viewStatus(status);
+  }
+
+  getEditorialById(idEditorial: number) {
+    this.editorialS.getEditorialById(idEditorial).subscribe((data) => {
+      this.dataEditorial = data[0];
+      this.idEditorial = this.dataEditorial.idEditorial;
+      this.formEditorial.setValue({
+        idEditorial: this.dataEditorial.idEditorial,
+        nameEditorial: this.dataEditorial.nameEditorial,
+        dateAdd: this.dataEditorial.dateAdd,
+        statusEditorial: this.dataEditorial.statusEditorial,
+      });
+    });
+  }
+
+  inactivateEditorial(idEditorial: number) {
+    this.editorialS.deleteEditorial(idEditorial).subscribe(() => {
+      this.getAlls();
+    });
+  }
+
+  activateEditorial(idEditorial: number) {
+    this.editorialS.putEditorialActivate(idEditorial).subscribe(() => {
+      this.getAlls();
+    });
+  }
 }
--- src/app/services/editorial/editorial.service.ts
+++ src/app/services/editorial/editorial.service.ts
@@ -1,25 +1,82 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, retry, throwError } from 'rxjs';
 import { editorialI } from 'src/app/interfaces/editorial.interface';
+import { responseI } from 'src/app/interfaces/response.interface';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EditorialService {
-
-  constructor(public http:HttpClient) { }
+  constructor(public http: HttpClient) {}
 
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + localStorage.getItem('token')
+      Authorization: 'Bearer ' + localStorage.getItem('token'),
     }),
   };
 
-  getEditorial():Observable<editorialI[]>{
+  getEditorial(): Observable<editorialI[]> {
     let direction = `${environment.uri}Editorial`;
-    return this.http.get<editorialI[]>(direction,this.httpOptions);
+    return this.http.get<editorialI[]>(direction, this.httpOptions).pipe(
+      retry(3),
+      map((resp) => resp),
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  getEditorialById(idEditorial: number): Observable<editorialI[]> {
+    let direction = `${environment.uri}Editorial/` + idEditorial;
+    return this.http.get<editorialI[]>(direction, this.httpOptions).pipe(
+      retry(3),
+      map((resp) => resp),
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  postEditorial(form: object): Observable<responseI> {
+    let direction = `${environment.uri}Editorial`;
+    return this.http.post<responseI>(direction, form, this.httpOptions).pipe(
+      map((resp) => resp),
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  putEditorial(form: object, idEditorial: number): Observable<responseI> {
+    let direction = `${environment.uri}Editorial/` + idEditorial;
+    return this.http.put<responseI>(direction, form, this.httpOptions).pipe(
+      map((resp) => resp),
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  putEditorialActivate(idEditorial: number): Observable<editorialI> {
+    let direction = `${environment.uri}Editorial/Activate/` + idEditorial;
+    return this.http.put<editorialI>(direction, '', this.httpOptions).pipe(
+      map((resp) => resp),
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  deleteEditorial(idEditorial: number): Observable<responseI> {
+    let direction = `${environment.uri}Editorial/` + idEditorial;
+    return this.http.delete<responseI>(direction, this.httpOptions).pipe(
+      map((resp) => resp),
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
   }
 }
