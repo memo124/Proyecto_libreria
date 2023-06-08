c09b36a6f8f6df02924e3956ac31df7c4e6ea044
AlexisMartínez <ia.alexismartinez@ufg.edu.sv>
2023-06-07 23:46:58 -0600

    validaciones login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login/login.service';
+import { classHelper } from 'src/app/helpers/helper';
+
+export class FormModel {
+  carnet: string;
+  username: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -17,10 +23,11 @@ export class LoginComponent {
 
 
 
-  constructor(private router:Router, private loginS:LoginService) {
+  constructor(private router:Router, private loginS:LoginService, private helper: classHelper) {
     this.loginForm = new FormGroup({
       userName : new FormControl('',Validators.compose([Validators.required])),
-      carnet : new FormControl('',Validators.required)
+      carnet : new FormControl('',Validators.required),
+      submitBtn: new FormControl
     })
   }
 
@@ -34,11 +41,34 @@ export class LoginComponent {
       }
   }
 
-  async onLogin(form:object){
-    this.loginS.postLogin(form).subscribe(data=>{
-       localStorage.setItem('token',data.toString());
-       this.router.navigate(['book']);
-    });
+  isSubmitting: boolean = false;
+
+  async onLogin(form:FormModel){
+    this.loginForm.disable();
+    this.isSubmitting = true;
+
+    if(form.carnet!="" && form.username!=""){
+
+      this.loginS.postLogin(form).subscribe(data=>{
+
+        if(data != undefined && data != null){
+         localStorage.setItem('token',data.toString());
+         this.router.navigate(['book']);
+        }
+        else{
+         this.helper.messageAlert('Ingrese credenciales validas','MotherFucker','warning','Oki');
+        }
+
+        this.loginForm.enable();
+        this.isSubmitting = false;
+     });
+    }
+    else{
+
+      this.helper.messageAlert('Pero ingresa algo por lo menos','TARADO','warning','Oki:c');
+      this.loginForm.enable();
+      this.isSubmitting = false;
+    }
   }
 
 }
